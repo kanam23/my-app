a2c5d465d8a69e0d64455987f97f68eb147edd94
kanam23 <Khairula0903@gmail.com>
2023-12-03 20:46:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; import './App.css'; import HelloWorld from './HelloWorld';
+import Counter from './Counter.jsx'; 
+class App extends Component { render() {
+  return (
+  <div className="App">
+  <HelloWorld name={`Khairul Anam`} />
+  <Counter />
+
+
+  </div>
+  );
+ }
+}
+export default App;
